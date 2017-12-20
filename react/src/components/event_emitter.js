6ed60b2745638a7d68662eaa773ee0eb48a22939
David <barrard@gmail.com>
2017-12-19 21:59:35 -1000

    ran the build

--- react/src/components/event_emitter.js
+++ react/src/components/event_emitter.js
@@ -23,6 +23,12 @@ class CustomEventEmitter {
 
 		this.events[event_name].forEach((i)=> i(event_details))
 	}
+	off(event_name, event_handler){
+		var index = this.events[event_name][event_handler]
+		this.events[event_name].splice(index, 1)
+		console.log('event_handler '+ event_handler +' removed')
+
+	}
 }
 
 let event_emitter = new CustomEventEmitter()
